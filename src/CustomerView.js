4a900149903417fa290b2642000fe16da35416c4
Boominathan <eboominathan@gmail.com>
2025-05-15 07:12:38 +0530

    Edit updated

--- src/CustomerView.js
+++ src/CustomerView.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { deleteCustomer } from "./slices/customerSlice";
+import { deleteCustomer, addCustomer } from "./slices/customerSlice";
 
 export default function CustomerView() {
   const customers = useSelector((state) => state.customers);
@@ -8,6 +8,14 @@ export default function CustomerView() {
   const deleteHandler = (index) => {
     dispatch(deleteCustomer(index));
   };
+  const editCustomer = (index) => () => {
+    const newName = prompt("Enter new name:", customers[index]);
+    if (newName) {
+      dispatch(deleteCustomer(index));
+      dispatch(addCustomer(newName));
+    }
+  };
+
   return (
     <div>
       <h3>Customer List</h3>
@@ -15,6 +23,7 @@ export default function CustomerView() {
         {customers.map((customer, index) => (
           <li key={index}>
             {customer}
+            <button onClick={editCustomer(index)}>Edit</button>
             <button onClick={() => deleteHandler(index)}>Delete</button>
           </li>
         ))}
